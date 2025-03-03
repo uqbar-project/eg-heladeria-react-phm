6abbe40e61e2d723046bb09d72f83bbce5c6be17
nicovio <nicolasviotti90@gmail.com>
2025-03-03 16:13:00 -0300

    Remove unnecessary callback

--- src/routes/_authenticated/editar-heladeria.$id.tsx
+++ src/routes/_authenticated/editar-heladeria.$id.tsx
@@ -1,11 +1,10 @@
 import heladeriaService from '@/service/heladeria-service'
-import { onBeforeLoad, onErrorRoute } from '@/utils/routes'
+import { onErrorRoute } from '@/utils/routes'
 import EditarHeladeria from '@/views/EditarHeladeria'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute(`/_authenticated/editar-heladeria/$id`)({
   component: EditarHeladeria,
   loader: async (params) => heladeriaService.fetchById(+params.params.id),
-  beforeLoad: onBeforeLoad,
   onError: onErrorRoute,
 })
