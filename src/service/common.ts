7c07fabd64ebd9c18bb67680a2e0377f009a688f
Fernando Dodino <fernando.dodino@gmail.com>
2025-03-07 20:40:04 -0300

    Fix XSRF token

--- src/service/common.ts
+++ src/service/common.ts
@@ -1,32 +1,18 @@
-import axios, { AxiosError, AxiosHeaders, AxiosRequestConfig } from 'axios'
-import { BACKEND_URL, TOKEN_KEY } from './constants'
-
-const getCookie = (name: string): string => {
-  const nameLenPlus = (name.length + 1)
-  console.info('document.cookie', document.cookie)
-  return document.cookie
-    .split(';')
-    .map(cookie => cookie.trim())
-    .filter(cookie => {
-      return cookie.substring(0, nameLenPlus) === `${name}=`
-    })
-    .map(cookie => {
-      return decodeURIComponent(cookie.substring(nameLenPlus))
-    })[0] || ''
-}
+import axios, { AxiosHeaders, AxiosRequestConfig } from 'axios'
+import { TOKEN_KEY } from './constants'
 
 export async function httpRequest<T>(request: AxiosRequestConfig): Promise<T> {
   const token = localStorage.getItem(TOKEN_KEY) ?? ''
+  const headers = request.headers as AxiosHeaders ?? new AxiosHeaders()
+  headers.setAuthorization(`Bearer ${token}`)
   const okRequest = {
     ...request,
-    method: request.method ?? 'GET',
-    headers: request.headers ?? new AxiosHeaders().setAuthorization(`Bearer ${token}`),
+    headers,
+    withXSRFToken: true,
+    withCredentials: true,
+    xsrfHeaderName: 'X-XSRF-TOKEN',
+    xsrfCookieName: 'XSRF-TOKEN',
   }
-  okRequest.withCredentials = true
-  // if (['POST', 'PUT'].includes(request.method!)) {
-  //   const csrfToken = getCookie('XSRF-TOKEN')
-  //   okRequest.headers!.append('X-XSRF-TOKEN', csrfToken)
-  // }
   const response = await axios(okRequest)
   return response.data
 }
