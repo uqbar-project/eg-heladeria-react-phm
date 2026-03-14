c562c5996a89dfcbf9cd988a2b0f876146ec9c90
nicovio <github@nicovio.dev>
2026-03-14 01:36:38 -0300

    Fix(Input): Generate unique input IDs using useId

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,6 @@
 import { twMerge } from 'tailwind-merge'
 import Label from '../Label'
-import { ReactNode, isValidElement } from 'react'
+import { ReactNode, isValidElement, useId } from 'react'
 
 type Props = {
   label?: ReactNode
@@ -8,13 +8,15 @@ type Props = {
 } & React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = ({ label, type = 'text', className, ...props }: Props) => {
-  const { id } = props
+  const generatedId = useId()
+  const id = props.id || generatedId
 
   return (
     <div className={twMerge('flex flex-col gap-2', className)}>
       {!!label && (isValidElement(label) ? label : <Label htmlFor={id}>{label}</Label>)}
       <input
         {...props}
+        id={id}
         className='rounded-lg border border-primary-200 bg-white px-3 py-2 text-sm text-primary-700 outline-none transition-all placeholder:text-primary-400 focus:border-accent-500 focus:ring-2 focus:ring-accent-500/15'
         type={type}
       />
